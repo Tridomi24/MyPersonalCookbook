c5d4b827f88246562b52247db82aa132af6d0b8e
Tridomi24 <t.mitchell3@ncl.ac.uk>
2016-04-08 15:57:36 +0100

    renamed viewRecipe to ViewRecipe

--- app/src/main/java/com/example/tmitchell/mypersonalcookbook/ViewRecipe.java
+++ app/src/main/java/com/example/tmitchell/mypersonalcookbook/ViewRecipe.java
@@ -1,13 +1,9 @@
 package com.example.tmitchell.mypersonalcookbook;
 
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.RelativeLayout;
 
-public class viewRecipe extends AppCompatActivity {
+public class ViewRecipe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

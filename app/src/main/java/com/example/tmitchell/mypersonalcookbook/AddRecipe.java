cd413fbb7a966036dbe196f9a671a417f8f2aff9
Tridomi24 <t.mitchell3@ncl.ac.uk>
2016-04-11 20:58:45 +0100

    Dropdown is now working.

--- app/src/main/java/com/example/tmitchell/mypersonalcookbook/AddRecipe.java
+++ app/src/main/java/com/example/tmitchell/mypersonalcookbook/AddRecipe.java
@@ -1,6 +1,5 @@
 package com.example.tmitchell.mypersonalcookbook;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -54,7 +52,7 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
                 android.R.layout.simple_spinner_dropdown_item, categories);
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner category = (Spinner) findViewById(R.id.addRecipe_category_in);
+        final Spinner category = (Spinner) findViewById(R.id.addRecipe_category_in);
         category.setAdapter(categoryAdapter);
 
         category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -69,8 +67,6 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
                 // TODO Auto-generated method stub
             }
         });
-
-        final String catChoice = (String) category.getSelectedItem();
 
         assert saveButton != null;
         saveButton.setOnClickListener(new View.OnClickListener()
@@ -91,12 +87,12 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
                                               String title = title_in.getText().toString();
                                               String imgPath = "";
                                               String servesStr = serves_in.getText().toString();
+                                              String catChoice = category.getSelectedItem().toString();
 
                                               //INTS
-
                                               final int servesNo = Integer.parseInt(servesStr);
 
-                                              db.addRecipe(new RecipeDB(0, title, "file path here",catChoice , servesNo,
+                                              db.addRecipe(new RecipeDB(0, title, imgPath, catChoice, servesNo,
                                                       "blah", "habba", "galarchi", 0, 0));
 
                                               startActivity(new Intent(AddRecipe.this,

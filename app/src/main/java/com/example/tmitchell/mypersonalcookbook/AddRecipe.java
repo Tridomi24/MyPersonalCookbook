e9cf607df2cf64be9f1f81a7ab16b0dc278f4ad7
Tridomi24 <t.mitchell3@ncl.ac.uk>
2016-05-11 11:54:51 +0100

    Dynamic ingredients list added, will now save as a string.

--- app/src/main/java/com/example/tmitchell/mypersonalcookbook/AddRecipe.java
+++ app/src/main/java/com/example/tmitchell/mypersonalcookbook/AddRecipe.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 
@@ -18,6 +22,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddRecipe extends AppCompatActivity implements View.OnClickListener {
+
+    /*********************************************
+     * Global counter for adding new ingredients
+     ********************************************/
+    private int counter = 0;
+    private LinearLayout ingredientLL;
+    private ArrayList<EditText> ingredientList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +44,16 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
 
         final DatabaseHandler db = new DatabaseHandler(this);
 
+
         /*********************************************
          Variables for adding into the Database
          ********************************************/
 
         LinearLayout ll = (LinearLayout) findViewById(R.id.addRecipe_LinearLayout);
+        ingredientLL = (LinearLayout) findViewById(R.id.ingredients_ll);
+
         final ScrollView sv = (ScrollView) findViewById(R.id.addRecipe_ScrollView);
+        final ImageButton addIngredientBtn = (ImageButton) findViewById(R.id.add_ingredient_button);
 
         Button saveButton = (Button) findViewById(R.id.addRecipe_submit_button);
         //CATEGORY DROPDOWN OPTIONS
@@ -47,6 +63,16 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
         categories.add("Dessert");
         categories.add("Snack");
 
+        /*********************
+         Add ingredient button
+         ********************/
+        assert addIngredientBtn != null;
+        addIngredientBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addIngredientEdit();
+            }
+        });
 
         //ADAPTER FOR SPINNER
         ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(this,
@@ -76,7 +102,7 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
                                            ************************/
                                           EditText title_in = (EditText) findViewById(R.id.addRecipe_title_in);
                                           EditText serves_in = (EditText) findViewById(R.id.addRecipe_serves_in);
-                                          EditText ingredients_in = (EditText) findViewById(R.id.addRecipe_ingredientList_in);
+                                          //EditText ingredients_in = (EditText) findViewById(R.id.addRecipe_ingredientList_in);
                                           EditText directions_in = (EditText) findViewById(R.id.addRecipe_Directions_in);
                                           EditText comments_in = (EditText) findViewById(R.id.addRecipe_Comments_in);
                                           EditText prep_in = (EditText) findViewById(R.id.addRecipe_preptime_in);
@@ -85,6 +111,21 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
 
                                           @Override
                                           public void onClick(View v) {
+                                              ArrayList<String> ingredients = new ArrayList<String>();
+
+                                              /*************************************************
+                                               Puts the ingredients values in to an array
+                                               ************************************************/
+                                              for (int i = 0; i < ingredientList.size(); i++) {
+                                                  ingredients.add(ingredientList.get(i).getText().toString());
+                                              }
+
+                                              String ingredientStr = "";
+
+                                              for (String s : ingredients) {
+                                                  ingredientStr += s + ",";
+                                              }
+
 
                                               //creates a validation object
                                               final Validation valid = new Validation();
@@ -101,7 +142,7 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
                                               String imgPath = "";
                                               String servesStr = serves_in.getText().toString();
                                               String catChoice = category.getSelectedItem().toString();
-                                              String ingredients = ingredients_in.getText().toString();
+                                              //String ingredients = ingredients_in.getText().toString();
                                               String directions = directions_in.getText().toString();
                                               String comments = comments_in.getText().toString();
                                               String prepStr = prep_in.getText().toString();
@@ -134,7 +175,7 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
                                                       && valid.reasonableTime(prepNo) && valid.reasonableTime(cookNo)) {
 
                                                   db.addRecipe(new RecipeDB(0, title, imgPath, catChoice, servesNo,
-                                                          ingredients, directions, comments, prepNo, cookNo));
+                                                          ingredientStr.toString(), directions, comments, prepNo, cookNo));
 
                                                   startActivity(new Intent(AddRecipe.this,
                                                           MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
@@ -157,5 +198,32 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
+    /**
+     * CODE TO ADD DYNAMIC EDIT TEXT BOXES ADAPTED FROM:
+     * http://findnerd.com/list/view/Create-EditText-on-button-click-in-Android/2723/
+     * [ACCESSED 01/05/16]
+     */
+    protected void addIngredientEdit() {
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+
+        counter++;
+
+        params.setMargins(0, 10, 0, 10);
+        EditText ingredient = new EditText(this);
+
+        ingredient.setId(counter);
+        ingredient.setInputType(InputType.TYPE_CLASS_TEXT);
+        ingredient.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        ingredient.setWidth(300);
+
+        ingredientLL.addView(ingredient);
+        ingredientList.add(ingredient);
+    }
+
 
 }
